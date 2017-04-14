0072f9a76a9e2341702a21b289c2ba959065ec40
Premnath Christopher <premnath.christopher@urssystems.com>
2017-04-14 19:37:00 +0530

    Added barcode generator

--- src/com/urs/controller/HomeController.java
+++ src/com/urs/controller/HomeController.java
@@ -1,22 +1,40 @@
 package com.urs.controller;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.WriterException;
+import com.google.zxing.client.j2se.MatrixToImageWriter;
+import com.google.zxing.common.BitMatrix;
+import com.google.zxing.oned.EAN13Writer;
+
 @Controller
 public class HomeController {
-	
+
 	private static final String BARCODE_ONE = "barcodeOne";
 
 	@Autowired
 	private Environment environment;
 
 	@GetMapping("/")
-	public String getHomePage(Model model) {
-		String barcode= environment.getRequiredProperty(BARCODE_ONE);
+	public String getHomePage(Model model) throws WriterException, FileNotFoundException, IOException {
+		String barcode = environment.getRequiredProperty(BARCODE_ONE);
+		int requestedWidth = 500;
+		int requestedHeight = 300;
+		String imageFormat = "png";
+		
+		BitMatrix bitMatrix = new EAN13Writer().encode(barcode, BarcodeFormat.EAN_13, requestedWidth, requestedHeight);
+		MatrixToImageWriter.writeToStream(bitMatrix, imageFormat,
+				new FileOutputStream(new File("E:\\EAN13.png")));
 		model.addAttribute("barcode", barcode);
 		return "index";
 	}
